dac9237419183a2e421ee28bd67f27ac10ccbbc9
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-02-04 07:49:16 -0800

    Iterates over full list; deletes unneeded or erroneous items; cleans up double-row elements where noun/verb/adj/whatever share row

--- scraper.py
+++ scraper.py
@@ -1,28 +1,43 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from string import ascii_uppercase
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-driver.get("https://anglish.fandom.com/wiki/English_Wordbook/A");
-content = driver.page_source;
-soup = BeautifulSoup(content, 'html.parser')
-tables = soup.findAll("table");
-for table in tables:
-	if table.findParent("table") is None:
-		table_body = table.find('tbody')
 
-rows = table_body.find_all('tr')
-rowtds = [row.findAll('td') for row in rows]
 anglish_dictionary = {};
+for letter in ascii_uppercase:
+	driver.get(f"https://anglish.fandom.com/wiki/English_Wordbook/{letter}");
+	content = driver.page_source;
+	soup = BeautifulSoup(content, 'html.parser')
+	tables = soup.findAll("table");
+	for table in tables:
+		if table.findParent("table") is None:
+			table_body = table.find('tbody')
 
-#word_columns = [rowtds.findAll('td') for rowtds_single in rowtds]
+	rows = table_body.find_all('tr')
+	rowtds = [row.findAll('td') for row in rows]
+	del rowtds[:2]	
+	def remove_bad_rows(row):
+		return len(row) > 3;	
+	rowtds = list(filter(remove_bad_rows, rowtds))
+	for i,row in enumerate(rowtds[:]):
+		if len(row) < 4:
+			previous_row_english = (rowtds[i-1])
+			print("less than 4")
+			rowtds[i].insert(0, previous_row_english)
+			print(rowtds[i])
 
-for word_column in rowtds[2:]:
-	english_def = word_column[0].text.strip();
-	class_type = word_column[1].text.strip();
-	attested = word_column[2].text.strip();
-	unattested = word_column[3].text.strip();
-	print({english_def:{"class":class_type, "attested":attested, "unattested":unattested}})
-	break;
-	#anglish_dictionary[englishdef:{"class":class_type, "attested":attested, "unattested":unattested}})
+	for word_column in rowtds:
+		print(len(word_column))
+		english_def = word_column[0].text.strip();
+		print(english_def)
+		class_type = word_column[1].text.strip();
+		print(class_type)
+		attested = word_column[2].text.strip();
+		print(attested)
+		unattested = word_column[3].text.strip();
+		print(unattested)
+		# print({english_def:{"class":class_type, "attested":attested, "unattested":unattested}})
+		anglish_dictionary.update({english_def:{"class":class_type, "attested":attested, "unattested":unattested}})
 
-#print(anglish_dictionary[6])
+print(anglish_dictionary["a cappella"])
 
