4cdc9a858a5c3fe35f457353938f033cd72fd7c0
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-02-03 18:30:41 -0800

    Finds table

--- scraper.py
+++ scraper.py
@@ -3,5 +3,8 @@ from bs4 import BeautifulSoup
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get("https://anglish.fandom.com/wiki/English_Wordbook/A");
 content = driver.page_source;
-
-print(content);
+soup = BeautifulSoup(content, 'html.parser')
+tables = soup.findAll("table");
+for table in tables:
+	if table.findParent("table") is None:
+		print(str(table))
